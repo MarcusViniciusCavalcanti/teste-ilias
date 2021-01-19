e12fc5d129b0c3bcac4d9262a1709db205e8319e
Venicius Cavalcanti <marcus.cavalcanti@matera.com>
2021-01-19 08:25:33 -0300

    build production endpoint

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrlCad: (path) => `http://cad:8080${path}`,
-  baseUrlAtm: (path) => `http://atm:8081${path}`,
+  baseUrlCad: (path) => `http://159.203.103.170:8080${path}`,
+  baseUrlAtm: (path) => `http://159.203.103.170/:8081${path}`,
 };
